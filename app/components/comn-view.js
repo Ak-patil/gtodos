2dfef484037bf5e72c751f1c3f9ebe63992ceebb
anandpatil4444 <anandpatil676@gmail.com>
2019-02-14 14:52:46 +0530

    c

--- app/components/comn-view.js
+++ app/components/comn-view.js
@@ -3,11 +3,12 @@ import Component from '@ember/component';
 export default Component.extend({
 //  number: 'one',
 //  numbersDisabled: true,
+router: Ember.inject.service(),
+
 actions: {
-    createnote(model)
+    saveModel(model)
     {  
         model.save().then((res) => {
-            debugger
             this.get('router').transitionTo('todos');
         }).catch((err) => {
             
@@ -16,25 +17,25 @@ actions: {
 
     },
   
-    savenote(todos) {
-      todos.save()
-      .then(() => this.transitionTo('todos'));
-    },
+    // savenote(todos) {
+    //   todos.save()
+    //   .then(() => this.transitionTo('todos'));
+    // },
 
-    willTransition(transition) {
+    // willTransition(transition) {
 
-      let model = this.controller.get('model');
+    //   let model = this.controller.get('model');
 
-      if (model.get('hasDirtyAttributes')) {
-        let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?");
+    //   if (model.get('hasDirtyAttributes')) {
+    //     let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?");
 
-        if (confirmation) {
-          model.rollbackAttributes();
-        } else {
-          transition.abort();
-        }
-      }
-    }
+    //     if (confirmation) {
+    //       model.rollbackAttributes();
+    //     } else {
+    //       transition.abort();
+    //     }
+    //   }
+    // }
   }
    
 
